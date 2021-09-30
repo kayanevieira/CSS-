95844a8f3b1b2c1e26cbd2d55166761181dca220
Kayane Vieira <kayane@startaideia.com.br>
2021-09-30 14:14:10 -0300

    added indentation

--- contato/index.js
+++ contato/index.js
@@ -1,17 +1,18 @@
 const form = document.getElementById("form");
 const inputSubmit = document.getElementById("submit-id");
 
-form.addEventListener("submit", function(event){
-    console.log("enviado");
-    const messageGreen = document.getElementById("d-none");
-    const elements = document.getElementsByName('input');
-    inputSubmit.disabled = true;
-    elements.forEach(element => {
-        element.classList.add('input-class');
-        element.style.border = "2px solid green";
-        messageGreen.style.display = "flex";
-        console.log(element.value);
-    })
-    event.preventDefault();
+form.addEventListener("submit", function (event) {
+  const messageGreen = document.getElementById("d-none");
+  const elements = document.getElementsByName("input");
 
-})
+  inputSubmit.disabled = true;
+
+  elements.forEach((element) => {
+    console.log(element.value);
+
+    element.classList.add("input-class");
+    element.style.border = "2px solid green";
+    messageGreen.style.display = "flex";
+  });
+  event.preventDefault();
+});
